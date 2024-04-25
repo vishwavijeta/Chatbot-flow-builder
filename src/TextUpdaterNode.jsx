4ed96249f855a3f9c8e15c363ee5d6f2dc1ad8b4
vishwa vijeta <vishwa@expertrec.com>
2024-04-25 22:26:54 +0530

    refactor TextUpdaterNode component

--- src/TextUpdaterNode.jsx
+++ src/TextUpdaterNode.jsx
@@ -1,22 +1,19 @@
-import { useCallback, useState } from 'react';
 import { Handle, Position } from 'reactflow';
 import { FcCallback } from "react-icons/fc";
-import { FcSms } from "react-icons/fc";
 import { BiMessageRoundedDetail } from "react-icons/bi";
+import './text-updater-node.css';
 
 
+const TextUpdaterNode = ({ data, isConnectable, id }) => {
 
+  const addActiveClass = (event) => {
 
-const handleStyle = { left: 10 };
-
-function TextUpdaterNode({ data, isConnectable, id }) {
-
-  const [isActive, setIsActive] = useState(false);
-
-
-  const addClass = (event) => {
-    console.log('clicked', event.currentTarget);
+    // This function is called when the node is clicked
+    // give cuurentTarget the value of the event.currentTarget
     const element = event.currentTarget;
+
+    // add the class 'active' to the element after 0ms
+    // this is done to add the class after the removeActiveClass function is called in App.js
     setTimeout(() => {
       element.classList.add('active');
     }, 0);
@@ -24,14 +21,14 @@ function TextUpdaterNode({ data, isConnectable, id }) {
 
 
   return (
-    <div id={id} className='text-updater-node' onClick={addClass}>
+    <div id={id} className='text-updater-node' onClick={addActiveClass}>
       <Handle type="target" position={Position.Left} isConnectable={isConnectable} />
-
       <div className='text-updater-node-header'>
-        <div style={{ display: 'flex' , alignItems: 'center' }}>
-          <BiMessageRoundedDetail className='message-icon'/> <span className='text-updater-node-header-text'>Send Message</span>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <BiMessageRoundedDetail className='message-icon' />
+          <span className='text-updater-node-header-text'>Send Message</span>
         </div>
-        <FcCallback  style={{fontSize: 'x-large'}} />
+        <FcCallback style={{ fontSize: 'x-large' }} />
       </div>
       <div className='text-updater-node-text'>
         {data}
@@ -39,6 +36,6 @@ function TextUpdaterNode({ data, isConnectable, id }) {
       <Handle type="source" position={Position.Right} id="b" isConnectable={isConnectable} />
     </div>
   );
-}
+};
 
 export default TextUpdaterNode;
